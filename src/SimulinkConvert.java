b0c39e0cffb09d244b646569a67f186307cb9547
Jin Ro <jinwooro@gmail.com>
2019-02-05 14:52:08 +1300

    backup

--- src/SimulinkConvert.java
+++ src/SimulinkConvert.java
@@ -20,8 +20,10 @@ import com.structure.SimQ.SimQssHA;
 
 import org.conqat.lib.simulink.model.stateflow.StateflowBlock;
 import org.conqat.lib.simulink.model.stateflow.StateflowChart;
+import org.conqat.lib.simulink.model.stateflow.StateflowDeclContainerBase;
 import org.conqat.lib.simulink.model.stateflow.StateflowMachine;
 import org.conqat.lib.simulink.model.stateflow.StateflowNodeBase;
+import org.conqat.lib.simulink.model.stateflow.StateflowTarget;
 import org.conqat.lib.simulink.model.stateflow.StateflowTransition;
 
 import java.util.HashMap;
@@ -30,13 +32,16 @@ import java.util.HashSet;
 public class SimulinkConvert {
 
 	public static void main(String[] args) throws SimulinkModelBuildingException, ZipException, IOException {
-	    File file = new File("resource/example1.slx");
+	    File file = new File("resource/example1.mdl");
 	    try (SimulinkModelBuilder builder = new SimulinkModelBuilder(file, new SimpleLogger())) {
 	    	 
 	    	SimulinkModel model = builder.buildModel();
 	    	HashSet<String> namesStateflowCharts = new HashSet<String>();
 	    	SimulinkUtils utils = new SimulinkUtils();
 	    	HashMap<String, SimQssHA> HAs = new HashMap<String, SimQssHA>();
+	    	
+	    	
+	    	
 	    	
 	    	// Iterate over the stateflow blocks first
 	    	for (StateflowChart chart : model.getStateflowMachine().getCharts()) {
@@ -63,20 +68,28 @@ public class SimulinkConvert {
 				System.out.println("Parameter Value: " + block.obtainLabelData().getText());
 	    	}
 	      
-	      // Extracting information of the stateflow charts. 
+	    	// Extracting information of the stateflow charts. 
 	      
-	      for (StateflowChart chart : model.getStateflowMachine().getCharts()) {
-	    	  System.out.println("Chart name: " + chart.getName());
-	    	  //System.out.println("Nodes: " + chart.getNodes().toString());
-	    	  for (StateflowNodeBase b : chart.getNodes()) {
-	    		  String[] parts = b.getResolvedId().split("\\r?\\n");
-	    		  System.out.println("Nodes: " + parts[0]);
-	    	  }
-	    		  
-	    	  for (StateflowTransition trans : utils.getAllTransitions(chart)) {
-	    		  System.out.println("Transition Text:" + trans.obtainLabelData().getText());
-	    	  }
-	      }
+	    	
+	    	System.out.println("All the targets: " + model.getStateflowMachine().getTargets().toString());
+	    	for (StateflowTarget a : model.getStateflowMachine().getTargets()) {
+	    		System.out.println("Each target: " + a.getName());
+	    	}
+	    	
+			for (StateflowChart chart : model.getStateflowMachine().getCharts()) {
+				System.out.println("Chart name: " + chart.getName());
+
+				for (StateflowNodeBase b : chart.getNodes()) {
+					String[] parts = b.getResolvedId().split("\\r?\\n");
+					System.out.println("Full String: " + b.getResolvedId().toString());
+					
+					System.out.println("Nodes: " + parts[0]);
+				}
+				
+				for (StateflowTransition trans : utils.getAllTransitions(chart)) {
+					System.out.println("Transition Text:" + trans.obtainLabelData().getText());
+				}
+			}
 	      	      
 	      
 	      // Distinguishing the duplicated reading process for stateflow chart
