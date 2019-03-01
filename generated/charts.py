158c5a506fa7145ec491a9ff92f3b762bdcde71f
jinwooro@gmail.com <jinwooro@Jins-MacBook-Pro.local>
2019-03-02 12:33:46 +1300

    gitignore test

--- generated/charts.py
+++
@@ -1,8 +0,0 @@
-class Chart:
-	#Internal continuous variables X_C
-	x = 0.0
-	#Internal discrete variables X_D
-	y = 0.0
-class Chart1:
-	#Internal continuous variables X_C
-	#Internal discrete variables X_D
